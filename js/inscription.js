ae9a54c648f626bbf2bbfa9565afb184317950a4
Nathan <nathanchapuis15@gmail.com>
2025-10-06 18:30:45 +0200

    Update inscription.js

--- js/inscription.js
+++ js/inscription.js
@@ -4,6 +4,7 @@ document.getElementById('registerForm')?.addEventListener('submit', async functi
     const email = document.getElementById('email').value.trim().toLowerCase();
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirmPassword').value;
+    
     if (password !== confirmPassword) {
         alert("❌ Les mots de passe ne correspondent pas.");
         return;
