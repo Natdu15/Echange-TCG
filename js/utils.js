4ecc4364d6c7994522c7a69857c9f86375a874a9
Nathan <nathanchapuis15@gmail.com>
2025-11-25 11:34:36 +0100

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,12 @@
+export async function sha256Hex(str) {
+  const buf = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(str));
+  return Array.from(new Uint8Array(buf))
+      .map(b => b.toString(16).padStart(2, '0'))
+      .join('');
+}
+
+export function generateSalt(length = 16) {
+  const array = new Uint8Array(length);
+  crypto.getRandomValues(array);
+  return Array.from(array).map(b => b.toString(16).padStart(2,'0')).join('');
+}
